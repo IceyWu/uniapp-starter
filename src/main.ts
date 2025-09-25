7ab2e9da72eb482ce168784e36add07bdc0c1b6f
IceyWu <3128006406@qq.com>
2025-09-25 17:00:44 +0800

    fix: update async component and import type definitions, adjust Vite configuration for plugin imports and enable UnoCSS

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createSSRApp } from 'vue'
 import store from '@/store'
 import App from './App.vue'
-import 'virtual:uno.css'
+import 'uno.css'
 
 export function createApp() {
   const app = createSSRApp(App).use(store)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
 import { resolve } from 'node:path'
-import uni from '@dcloudio/vite-plugin-uni'
+import Uni from '@uni-helper/plugin-uni'
 import Components from '@uni-helper/vite-plugin-uni-components'
 import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
 import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
@@ -11,153 +11,131 @@ import Optimization from '@uni-ku/bundle-optimizer'
 import AutoImportTypes from 'auto-import-types'
 import PiniaAutoRefs from 'pinia-auto-refs'
 import { UpResolver } from 'uni-ui-plus'
+import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import UniPolyfill from 'vite-plugin-uni-polyfill'
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => {
-  /**
-   * 为兼容 @dcloudio/vite-plugin-uni 采用 CJS ，而 UnoCSS 只支持 ESM
-   * @see https://github.com/dcloudio/uni-app/issues/4815
-   */
-  const Unocss = await import('unocss/vite').then(i => i.default)
-
-  return {
-    resolve: {
-      alias: {
-        '~/': `${resolve(__dirname, 'src')}/`,
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
     },
-    plugins: [
-      AutoImportTypes(),
-      PiniaAutoRefs(),
-      UniManifest(),
-      /**
-       * vite-plugin-uni-pages
-       * @see https://github.com/uni-helper/vite-plugin-uni-pages
-       */
-      UniPages({
-        exclude: ['**/components/**/*.*'],
-        subPackages: ['src/pages-sub', 'src/sub-packages'],
-        dts: './src/@types/uni-pages.d.ts',
-      }),
-
-      /**
-       * vite-plugin-uni-layouts
-       * @see https://github.com/uni-helper/vite-plugin-uni-layouts
-       */
-      UniLayouts(),
-
-      /**
-       * unocss
-       * @see https://github.com/antfu/unocss
-       * see unocss.config.ts for config
-       */
-      Unocss(),
-
-      /**
-       * unplugin-auto-import 按需 import
-       * @see https://github.com/antfu/unplugin-auto-import
-       */
-      AutoImport({
-        imports: [
-          'vue',
-          'uni-app',
-          'pinia',
-          'vue-i18n',
-          {
-            '@iceywu/utils': [
-              'to',
-              'list',
-              'sleep',
-              'consolePlus',
-              'getObjVal',
-              'isArray',
-              'isEmpty',
-              'isObject',
-              'isNumber',
-              'isString',
-              'arrayLast',
-              'arrayFirst',
-              'deepClone',
-              'customDestr',
-              'toPro',
-            ],
-          },
-          {
-            'vue-hooks-pure': [
-              'useRequest',
-            ],
-          },
-        ],
-        dts: './src/@types/auto-imports.d.ts',
-        dirs: ['./src/composables', './src/api', './src/store', './src/utils/http', './src/hooks'],
-        vueTemplate: true,
-        // packagePresets: ['@iceywu/utils', 'vue-hooks-pure'],
-      }),
-
-      /**
-       * unplugin-vue-components 按需引入组件
-       * 注意：需注册至 uni 之前，否则不会生效
-       * @see https://github.com/antfu/vite-plugin-components
-       */
-      Components({
-        resolvers: [
-          UpResolver(),
-          WotResolver(),
-        ],
-        dts: './src/@types/components.d.ts',
-      }),
-
-      /**
-       * vite-plugin-vue-devtools 增强 Vue 开发者体验
-       * @see https://github.com/webfansplz/vite-plugin-vue-devtools
-       */
-      VueDevTools(),
-
-      uni(),
-
-      Optimization({
-        enable: {
-          'optimization': true,
-          'async-import': true,
-          'async-component': true,
-        },
-        dts: {
-          'enable': true,
-          'base': 'src/@types',
-          // 上面是对类型生成的比较全局的一个配置
-          // 下面是对每个类型生成的配置，以下各配置均为可选参数
-          'async-import': {
-            enable: true,
-            base: 'src/@types',
-            name: 'async-import.d.ts',
-            path: 'src/@types/async-import.d.ts',
-          },
-          'async-component': {
-            enable: true,
-            base: 'src/@types',
-            name: 'async-component.d.ts',
-            path: 'src/@types/async-component.d.ts',
-          },
-        },
-        logger: true,
-      }),
-    ],
+  },
+  plugins: [
+    AutoImportTypes(),
+    PiniaAutoRefs(),
+    UniManifest(),
+    /**
+     * vite-plugin-uni-pages
+     * @see https://github.com/uni-helper/vite-plugin-uni-pages
+     */
+    UniPages({
+      exclude: ['**/components/**/*.*'],
+      subPackages: ['src/pages-sub', 'src/sub-packages'],
+      dts: './src/@types/uni-pages.d.ts',
+    }),
 
     /**
-     * Vitest
-     * @see https://github.com/vitest-dev/vitest
+     * vite-plugin-uni-layouts
+     * @see https://github.com/uni-helper/vite-plugin-uni-layouts
      */
-    test: {
-      environment: 'jsdom',
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
+    UniLayouts(),
+
+    UnoCSS(),
+
+    /**
+     * unplugin-auto-import 按需 import
+     * @see https://github.com/antfu/unplugin-auto-import
+     */
+    AutoImport({
+      imports: [
+        'vue',
+        'uni-app',
+        'pinia',
+        'vue-i18n',
+        {
+          '@iceywu/utils': [
+            'to',
+            'list',
+            'sleep',
+            'consolePlus',
+            'getObjVal',
+            'isArray',
+            'isEmpty',
+            'isObject',
+            'isNumber',
+            'isString',
+            'arrayLast',
+            'arrayFirst',
+            'deepClone',
+            'customDestr',
+            'toPro',
+          ],
+        },
+        {
+          'vue-hooks-pure': [
+            'useRequest',
+          ],
+        },
+      ],
+      dts: './src/@types/auto-imports.d.ts',
+      dirs: ['./src/composables', './src/api', './src/store', './src/utils/http', './src/hooks'],
+      vueTemplate: true,
+      // packagePresets: ['@iceywu/utils', 'vue-hooks-pure'],
+    }),
+
+    /**
+     * unplugin-vue-components 按需引入组件
+     * 注意：需注册至 uni 之前，否则不会生效
+     * @see https://github.com/antfu/vite-plugin-components
+     */
+    Components({
+      resolvers: [
+        WotResolver(),
+        UpResolver(),
+      ],
+      dts: './src/@types/components.d.ts',
+    }),
+
+    UniPolyfill(),
+
+    Optimization({
+      enable: {
+        'optimization': true,
+        'async-import': true,
+        'async-component': true,
+      },
+      dts: {
+        'enable': true,
+        'base': 'src/@types',
+        // 上面是对类型生成的比较全局的一个配置
+        // 下面是对每个类型生成的配置，以下各配置均为可选参数
+        'async-import': {
+          enable: true,
+          base: 'src/@types',
+          name: 'async-import.d.ts',
+          path: 'src/@types/async-import.d.ts',
+        },
+        'async-component': {
+          enable: true,
+          base: 'src/@types',
+          name: 'async-component.d.ts',
+          path: 'src/@types/async-component.d.ts',
         },
       },
-    },
-  }
+      logger: true,
+    }),
+    Uni(),
+  ],
+
+  /**
+   * Vitest
+   * @see https://github.com/vitest-dev/vitest
+   */
+  test: {
+    environment: 'jsdom',
+  },
+
 })
